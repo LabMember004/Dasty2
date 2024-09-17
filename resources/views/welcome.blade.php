bf637b044cf0fce2195446b2822956d6b2e6e3cc
Heshar <hesharvaldez@gmail.com>
2024-09-17 23:04:51 +0300

    Design header and filter is finished

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,9 +11,9 @@
     <x-filter />
    
     <div class="flex border-2 items-center border-black w-1/4 justify-between mr-auto ml-auto h-12">
-    <a href="" class="inline-block px-6 py-3 bg-gray-100  hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-opacity-50 transition duration-200">Newest Items</a>
-    <a href="" class="inline-block px-6 py-3">My Items</a>
-    <a href="">Add Items</a>
+    <a href="" class="inline-block px-6 py-3 font-semibold rounded-lg bg-gray-100  hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-opacity-50 transition duration-200">Newest Items</a>
+    <a href="" class="inline-block px-6 py-3 bg-blue-400 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-opacity-50 trasition duration-200">My Items</a>
+    <a href="" class="inline-block px-6 py-3 bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-300 focus:ring-opacity-50 trasition duration-200">Add Items</a>
 </div>
     <div class="mt-10">
         <div class="product-group">
